5e09b84af56a54e72fec24edfa07670347169aec
sixtebansito <dannygghhm@gmail.com>
2026-09-11 15:47:57 -0500

    Limpiar archivos de prueba y mover scripts a prisma/

--- query2.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.usuarios.findMany({ include: { rector: true, inspector: true, profesor: true } });
-  console.log(users.map(u => ({ id: u.UsuarioID, cedula: u.Cedula, isRector: !!u.rector, isInspector: !!u.inspector })));
-}
-main().finally(() => prisma.$disconnect());
--- scratch/testlogin.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-http.get('http://localhost:3000/login', res => {
-  console.log('Status:', res.statusCode);
-  if (res.headers.location) {
-    console.log('Location:', res.headers.location);
-  }
-});
